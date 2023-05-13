9d9ad645f85380828fa7e1b1c7088c12562fe034
huygamcha <lehuynhhuy2002@gmail.com>
2023-05-13 22:57:19 +0700

    add js

---
+++ code.js
@@ -0,0 +1,8 @@
+var htmls = document.querySelector('#content-header-date-realtime')
+var currentTime = new Date()
+var day = currentTime.getDay()+1
+var date = currentTime.getDate()
+var month = currentTime.getMonth()+1
+var year = currentTime.getFullYear()
+var format = 'Thứ '+ day + ', ' + date + ' tháng ' + month + ', ' + year
+htmls.innerHTML = format
